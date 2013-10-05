d462cc3820e8ddc8e016c2a81dcade0a30ca68c0
Starik <4analit@gmail.com>
2013-10-05 22:54:28 +0400

    Add options and append this options to layer tiles

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var LeafletMap = function(mapId){
     this.mapData;
     this.mapTylesLayer;
 
-    this.appendMapData = function(data){
+    this.appendMapOptions = function(data){
 
      }
 
@@ -46,21 +46,61 @@ var LeafletTiles = function(){
 
     this.layer;
 
+    this.server;
+    this.tilesURL;
+    this.maxZoom;
+    this.minZoom;
+
+    // TODO: сделать проверку что это строка и ссылка
+    this._validateTilesURL = function(url){
+        if (url) {
+            return url;
+        }
+        else {
+            return "http://{s}.tiles.mapbox.com/v3/examples.map-y7l23tes/{z}/{x}/{y}.png";
+        }
+     }
+
+    this.setLayerOptions = function(data){
+        this.server = data.server && data.server in ["img", "wms"] ? data.server : "img";
+        this.tilesURL = this._validateTilesURL(data.tilesURL);
+        this.maxZoom = data.maxZoom ? data.maxZoom : 18;
+        this.minZoom = data.minZoom ? data.minZoom : 1;
+     }
+
     this.setLayer = function(url){
-        url = url ? url : 'http://tile.openstreetmap.org/{z}/{x}/{y}.png';
+        url = url ? url : this.tilesURL? this.tilesURL : this._validateTilesURL();
         this.layer = L.tileLayer(url);
-    }
+     }
 
 }
+
+var Options = function(){
+
+    this.maps = {
+        "cloudmate": {
+            tags: ["0. Современные онлайн карты"],
+            src: "Internet",
+            server: "img",
+            title: 'Cloudmate',
+            tilesURL: 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
+            maxZoom: 18,
+            minZoom: 0,
+            startZoom: 13,        
+        },
+    };
+
+}
+
+var opt = new Options();
+
+var layer = new LeafletTiles();
+layer.setLayerOptions(opt.maps.cloudmate);
+layer.setLayer();
 
 var map = new LeafletMap("map1");
 map.createMap();
 map.setMapCenter();
 map.setMapZoom();
-
-var layer = new LeafletTiles();
-layer.setLayer();
 map.setMapTilesLayer(layer);
 
-layer.setLayer("http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png");
-map.setMapTilesLayer(layer);
