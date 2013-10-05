4341f240d995ba960a2c385bb507f8d45cff8322
Starik <4analit@gmail.com>
2013-10-05 16:30:02 +0400

    Add map functions

--- js/main.js
+++ js/main.js
@@ -5,8 +5,37 @@ var LeafletMap = function(mapId){
     if (!mapId && !$("#"+mapId)){ return }
     this.mapId = mapId;
 
-    this.map = L.map(mapId).setView([51.505, -0.09], 13);
+    this.mapData;
 
-}
+    this.appendMapData = function(data){
+
+     }
+
+    this.createMap = function(){
+        this.map = L.map(this.mapId);
+     }
+
+    this.setMapCenter = function(latlng){
+        latlng = latlng ? latlng : [51.505, -0.09];
+        this.map.panTo(latlng);
+     }
+
+    this.setMapZoom = function(zoom){
+        zoom = zoom ? zoom : 13;
+        this.map.setZoom(zoom);
+     }
+    
+    this.setMapTiles = function(){
+
+     }
+
+    this.refreshMapAfterResize = function(){
+        this.map.invalidateSize();
+     }
+
+ }
 
 var map = new LeafletMap("map1");
+map.createMap();
+map.setMapCenter();
+map.setMapZoom();
