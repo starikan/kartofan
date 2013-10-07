24f52eca991db564f8c926d065e375591ac44b67
Starik <4analit@gmail.com>
2013-10-07 20:38:19 +0400

    Add: GruntJS

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+//Стандартный экспорт модуля в nodejs
+module.exports = function(grunt) {
+    // Инициализация конфига GruntJS
+    grunt.initConfig({
+
+        //Настройки различных модулей GruntJS, их нужно предварительно установить через менеджер пакетов npm, 
+        // или добавить в файл package.json перед запуском npm install
+
+        //Например проверка кода javascript с помощью утилиты jshint
+        jshint: {},
+
+        //Склеивание файлов
+        concat: {}
+
+        jade: {
+
+        }
+    });
+
+    //Загрузка модулей, которые предварительно установлены
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+
+    //Эти задания будут выполнятся сразу же когда вы в консоли напечатание grunt, и нажмете Enter
+    // grunt.registerTask('default', ['jshint', 'concat']);
+    grunt.registerTask('default', []);
+ };
