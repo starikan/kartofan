79991196401fd9cb72012208c53ee5c484b11c94
Starik <4analit@gmail.com>
2013-12-03 11:42:51 +0400

    Map edit menu refactoring

--- source/js/starik.kartofan.events.js
+++ source/js/starik.kartofan.events.js
@@ -68,10 +68,10 @@ var Events = (function(){
             parent.mapLocalGeneratedMenu.groupedCollectionMenu(opt.getOption("maps"), mapsEditor.editMap, true, "group");
         }},        
         { type: "line", text: "Get External Maps", callback: function(){
-            parent.createExternalJSONMenu("maps", function(i, v){ parent.setActiveMap(i, v) })
+            parent.createExternalJSONMenu("maps", function(i, v){ mapsEditor.setMap(i, v) })
          }},
         { type: "line", text: "Add Selected Map To Storage", callback: function(){
-            parent.editMap();
+            mapsEditor.editMap();
         }},
         { type: "line", text: "Get All Maps From JSON", callback: function(){
             mapsEditor.getAllMapsJSON();
@@ -79,19 +79,19 @@ var Events = (function(){
 
         { type: "paragraf", text: "Stage" },
         { type: "line", text: "Set Stage", callback: function(){
-            parent.createLocaleSelectMenu("stages", function(i, v){ parent.loadStage(i) });
+            parent.mapLocalGeneratedMenu.groupedCollectionMenu(opt.getOption("stages"), stageEditor.loadStage, true, "group");
         }},
         { type: "line", text: "Edit Stages", callback: function(){
-            parent.createLocaleSelectMenu("stages", parent.editStage, "Select To Edit Stage Data");
+            parent.mapLocalGeneratedMenu.groupedCollectionMenu(opt.getOption("stages"), stageEditor.editStage, true, "group");
         }},         
         { type: "line", text: "Edit Stage View", callback: function(){
             stageEditor.editView();
         }},
         { type: "line", text: "Save Stage", callback: function(){
-            parent.saveStage();
+            stageEditor.saveStage();
         }},
         { type: "line", text: "Load External Stage", callback: function(){
-            parent.createExternalJSONMenu("stages", function(i, v){ parent.loadStage("", v) })
+            parent.createExternalJSONMenu("stages", function(i, v){ stageEditor.loadStage("", v) })
         }},        
 
         { type: "paragraf", text: "Options" },
@@ -186,55 +186,6 @@ var Events = (function(){
 
 
 
-    this.setActiveMap = function(mapName, mapData){
-        mapName = mapName ? mapName : "";
-        var mapNum = opt.getOption("current", "activeMap");
-        window[mapNum].setMapTilesLayer(new LeafletTiles(mapName, mapData));
-     }
-
-    this.createLocaleSelectMenu = function(collection, callback, header){
-
-        var allData = opt.getOption(collection);
-
-        var groups = $.pluck(allData, 'group');
-        groups = $.unique(groups);
-        groups.sort();
-
-        var genArray = [{ type: "header", text: header }];
-
-        $.each(groups, function(g, group){
-
-            var dataInGroup = {};
-            $.each(allData, function(i, v){
-                if (!v.group && !group || v.group == group){
-                    dataInGroup[i] = v;
-                }
-                // if (v.group == group) {
-                //     dataInGroup[i] = v;
-                // }
-            })
-
-            if (!$.isEmptyObject(dataInGroup)){
-                
-                if (!group) {group = "Others"};
-                genArray.push({ type: "paragraf", text: group });
-                
-                $.each(dataInGroup, function(i, vi){
-                    console.log(i, vi)
-                    genArray.push({
-                        type: "line", 
-                        text: vi.title ? vi.title : "Noname",
-                        callback: function(){
-                            callback(i);
-                        },
-                    })
-                });
-            }
-        })
-
-        new CSSMenu("mapSelectMenu", genArray, true);
-     }
-
     this.createExternalJSONMenu = function(collection, callback, header){
 
         var extData = opt.getOption("global", "externalFeeds");
@@ -266,80 +217,6 @@ var Events = (function(){
                 menu.makeFromObj(genArr);
             })
         });
-     }
-
-    this.editMap = function(mapId){
-
-
-        var maps = opt.getOption("maps");
-        var mapVals;
-
-        var _deleteMapFunc = function(form){
-            if (confirm(loc("editMaps:deleteMap", mapVals.id))) {
-                if (mapVals.id){
-                    form.hideForm();
-                    opt.deleteOption("maps", mapVals.id);
-                    console.log(mapVals.id + "deleted")
-                }
-            }                    
-         }
-
-        var _submitMapFunc = function(form){
-            form.getAllData(); 
-            if (!form.checkForm){
-                alert(loc("editMaps:errorCheckForm"));
-                return;
-            }
-
-            if (opt.getOption("maps", form.allData.val.id)){
-                if (!confirm(loc("editMaps:mapRewriteConfirm", form.allData.val.id))) {
-                    return;
-                }
-            }
-
-            form.hideForm();
-            opt.setOption("maps", form.allData.val.id, form.allData.val)
-            console.log(form.allData.val.id, opt.getOption("maps", form.allData.val.id));            
-         }
-
-        var eformFunc = {
-            "submit": { "events": "click", callback: _submitMapFunc },
-            "delete": { "events": "click", callback: function(form){_deleteMapFunc(form)} },
-            "cancel": { "events": "click", callback: function(form){form.hideForm()} }
-         }
-
-
-        // If no mapId get active map
-        if (!mapId){
-            var mapNum = opt.getOption("current", "activeMap");
-            mapVals = window[mapNum].mapTilesLayer.mapData;
-            console.log(mapVals)
-            mapVals.id = window[mapNum].mapTilesLayer.mapName;
-        }
-        else {
-            mapVals = opt.getOption("maps", mapId);
-            mapVals.id = mapVals.id ? mapVals.id : mapId;
-        }
-
-        // Groups suggestions
-        var mapOptions = {}
-        var tempGroups = []
-        $.each(maps, function(i, v){
-            if (v.group){
-                tempGroups.push(v.group)
-            }
-        })
-        tempGroups = $.unique(tempGroups);
-        tempGroups.sort()
-        mapOptions.group = tempGroups;        
-
-        // Generate Form
-        $.getJSON("data/map_edit_form.json", function(eformFields){
-            eform = new EditableForm("addMap", eformFields, eformFunc);
-            eform.fillForm(mapVals, mapOptions);
-            
-            console.log(eform.allData);
-        }); 
      }
 
  }}());
